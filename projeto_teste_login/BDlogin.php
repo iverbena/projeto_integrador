bac793fa6f02968d2ec319fec3d4fc6512fb03cd
Gu1lh3rm3Fr4nca <88013049+Gu1lh3rm3Fr4nca@users.noreply.github.com>
2024-05-13 05:18:35 -0300

    Add files via upload
    implementado php e acesso ao banco de dados

---
+++ projeto_teste_login/BDlogin.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include 'login.php'; // incluir o arquivo de conexão
+
+// Obter os dados do formulário de login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Consulta para verificar se o usuário existe
+$sql = "SELECT * FROM usuarios WHERE nome='$username' AND senha='$password'";
+$result = mysqli_query($conn, $sql);
+
+// Verificar se a consulta retornou algum resultado
+if (mysqli_num_rows($result) == 1) {
+    // Login bem-sucedido
+    $_SESSION['username'] = $username; // salvar o nome de usuário na sessão
+    header('Location: ../pagina_inicial/pagina_inicial.php'); // redirecionar para a página do painel de controle
+} else {
+    // Login falhou
+    echo "Nome de usuário ou senha incorretos.";
+}
+
+mysqli_close($conn); // fechar a conexão
+?>
---
+++ projeto_teste_login/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section style="background-color: lightgrey;">
+        <h1>LOGIN</h1>
+        <form action="BDlogin.php" method="post">
+            <label for="usuario">Usuario: </label>
+            <input type="text" name="username" id="nome">
+            <br>
+            <label for="senha">Senha:</label>
+            <input type="password" name="password" id="senha">
+            <br>
+            <input type="submit" name="login" value="Login" style="width: 21%;">
+        </form>
+        <form action="../pagina_de_cadastro/cadastro.php" method="post">
+        <input type="submit" name="cadastro" value="cadastro" style ='width: 28%;'>
+        </form>
+    </section>
+</body>
+</html>
