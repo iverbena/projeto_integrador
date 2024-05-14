a40bbf7dbad1fd2a49c1d2a3248eba248ae3ff9b
Gu1lh3rm3Fr4nca <88013049+Gu1lh3rm3Fr4nca@users.noreply.github.com>
2024-05-14 09:45:33 -0300

    Add files via upload
    update

---
+++ projeto_sucesso_cadastro/sucesso_cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sucesso!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body">
+    <section style="background-color: lightgrey;">
+        <h1>CADASTRO REALIZADO COM SUCESSO!</h1>
+        <div id="imagem">
+            <img src="../Imagens/joinha.png" alt="" width="60%">
+        </div>
+        <button><a href="../projeto_teste_login/index.php">Fazer Login</a></button>
+    </section>
+</body>
+</html>
