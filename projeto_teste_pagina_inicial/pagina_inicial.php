6fbd5396fc176d0df326b1e13523854301429a88
Gu1lh3rm3Fr4nca <88013049+Gu1lh3rm3Fr4nca@users.noreply.github.com>
2024-05-14 09:47:18 -0300

    Add files via upload
    update

--- projeto_teste_pagina_inicial/pagina_inicial.php
+++ projeto_teste_pagina_inicial/pagina_inicial.php
@@ -3,43 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
-        <h1>Nome Do Sistema</h1>
-        <h2>subtitulo</h2>
-        <br>
+        <h1>ENCONTRE A SUA ALCATEIA</h1>
+        <img id="logo" src="../Imagens/logo.png" alt="">
+        <br><br><br>
         <nav>
             <li>
-                <ul>HOME</ul>
-                <ul>ITEM 02</ul>
-                <ul>ITEM 03</ul>
-                <ul>PESQUISA</ul>
+                <ul><button>HOME</button></ul>
+                <ul><button>PERFIL</button></ul>
+                <ul><button>EQUIPES</button></ul>
+                <ul><button>ARTIGOS</button></ul>
+                <ul><button>PESQUISA</button></ul>
             </li>
         </nav>
     </header>
     <section id="barra_lateral">
         <!--Imagem de perfil-->
-        <img src="" alt="">
+        <img src="../Imagens/perfil.png" alt="" width="100%">
         <p>Nome de usuario</p>
-        <li>
-            <ul>perfil</ul>
-            <ul>jogos</ul>
-            <ul>equipes</ul>
-            <ul>artigos</ul>
-            <ul>tutoriais</ul>
-        </li>
     </section>
     <section id="conteudo_principal">
         <article>
-            <label for="artigo">COMPARTILHE ALGO:</label>
-            <input type="text" name="artigo" id="artigo">
+            <h3>EQUIPES</h3>
+            <div id="equipe">
+                <button>Criar Equipe</button>
+                <button>Procurar Equipe</button>
+            </div>
         </article>
-        <BR></BR>
+        <br><br>
         <article>
-            <h3>ULTIMAS POSTAGENS</h3>
+            <h3>ARTIGOS</h3>
+            <div id="equipe">
+                <button>Criar Artigo</button>
+                <button>Procurar Artigo</button>
+            </div>
         </article>
     </section>
 </body>
