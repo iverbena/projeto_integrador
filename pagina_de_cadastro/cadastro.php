a800327ed585273adc1fbf59692108387dd98d7b
Antonio Marcos Mello <145564252+Melloant@users.noreply.github.com>
2024-05-23 20:11:03 -0300

    Update cadastro.php
    update

--- pagina_de_cadastro/cadastro.php
+++ pagina_de_cadastro/cadastro.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="cadastro.css">
 </head>
 <body">
-    <header style="text-align: center;">
+    <header id="header">
         <img src="../Imagens/logo.png" alt="" id="logo">
         <p id="subtitulo">ENCONTRE SUA ALCATEIA</p>
     </header>
-    <section style="background-color: lightgrey;">
-        <h1>Registre-se</h1>
+    <section id="form" style="background-color: lightgrey;">
+        <h1>Registre-se:</h1>
         <form action="cadastrar_usuario.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
-        <input type="submit" value="Cadastrar">
-    </form>
+            <label for="nome">Nome:</label>
+            <input class="inputs" type="text" id="nome" name="nome" required><br><br>
+            <label for="email">Email:</label>
+            <input class="inputs" type="email" id="email" name="email" required><br><br>
+            <label for="senha">Senha:</label>
+            <input class="inputs" type="password" id="senha" name="senha" required><br><br>
+            <input type="submit" value="Cadastrar">
+        </form>
     </section>
     <div id="imagem">
         <img id= 'boneco' src="../Imagens/fortnite_boneco.png" alt="">
